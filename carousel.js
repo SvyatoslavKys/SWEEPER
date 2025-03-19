eee79c8147b2defdea622b4ec33545e01bfa37c8
SvyatoslavKys <158611727+SvyatoslavKys@users.noreply.github.com>
2025-03-19 21:29:12 +0100

    added courusel
    added courusel

---
+++ carousel.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $('.autoplay').on('mouseenter', function() {
+        $(this).slick('slickPause');
+    });
+
+    $('.autoplay').on('mouseleave', function() {
+        $(this).slick('slickPlay');
+    });
+});
